2df2973f078ab3590dc0b9dbf0f1d5cf386d4add
isaacBullinger <isaac.k.bullinger@gmail.com>
2025-05-09 11:34:33 -0500

    Added Cool Pics and included responsive design.

---
+++ coolpics/scripts/components.js
@@ -0,0 +1,29 @@
+// Load header and footer for each page.
+document.getElementById("header").innerHTML = 
+    fetch("../pages/components/header.html")
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById("header").innerHTML = html
+        });
+
+document.getElementById("footer").innerHTML = 
+    fetch("../pages/components/footer.html")
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById("footer").innerHTML = html
+        });
+
+// For loop that dynamically generates the photos.
+const numPhotos = 9;
+let photoGallery = document.getElementById("gallery");
+
+fetch("../pages/components/figure.html")
+    .then(response => response.text())
+    .then(html => {
+        let content = "";
+
+        for (let i = 0; i < numPhotos; i++) {
+            content += html;
+        }
+        photoGallery.innerHTML = content;
+    });
