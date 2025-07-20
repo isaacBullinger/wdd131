21da96320b1456b8f4cfec89113bc5440ca581b7
isaacBullinger <isaac.k.bullinger@gmail.com>
2025-07-19 19:45:35 -0500

    Fixed path issue.

--- portfolio/scripts/elements.js
+++ portfolio/scripts/elements.js
@@ -1,3 +1,12 @@
+const pathPrefix = location.pathname.includes("/pages/") ? "../" : "";
+fetch(`${pathPrefix}pages/components/header.html`)
+  .then(res => res.text())
+  .then(html => document.getElementById("header").innerHTML = html);
+
+fetch(`${pathPrefix}pages/components/footer.html`)
+  .then(res => res.text())
+  .then(html => document.getElementById("footer").innerHTML = html);
+
 fetch("/portfolio/pages/components/header.html")
     .then(response => response.text())
     .then(
